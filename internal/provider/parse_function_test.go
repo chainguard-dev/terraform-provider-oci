5a02b0be969b61c736ea9aa8143fe4393a5f0ad0
Jason Hall <jason@chainguard.dev>
2024-05-05 14:02:30 -0400

    new function: parse
    Signed-off-by: Jason Hall <jason@chainguard.dev>

---
+++ internal/provider/parse_function_test.go
@@ -0,0 +1,82 @@
+package provider
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+)
+
+func TestParseFunction(t *testing.T) {
+	// A naked ref string errors due to missing digest
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{{
+			Config:      `output "parsed" { value = provider::oci::parse("") }`,
+			ExpectError: regexp.MustCompile(""), // any error is ok
+		}},
+	})
+
+	// A fully qualified tag ref string errors due to missing digest
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{{
+			Config:      `output "parsed" { value = provider::oci::parse("cgr.dev/foo/sample:latest") }`,
+			ExpectError: regexp.MustCompile(""), // any error is ok
+		}},
+	})
+
+	// A fully qualified ref
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{{
+			Config: `output "parsed" { value = provider::oci::parse("cgr.dev/foo/sample@sha256:1234567890123456789012345678901234567890123456789012345678901234") }`,
+			ConfigStateChecks: []statecheck.StateCheck{
+				statecheck.ExpectKnownOutputValue("parsed", knownvalue.ObjectExact(map[string]knownvalue.Check{
+					"registry":      knownvalue.StringExact("cgr.dev"),
+					"repo":          knownvalue.StringExact("foo/sample"),
+					"registry_repo": knownvalue.StringExact("cgr.dev/foo/sample"),
+					"digest":        knownvalue.StringExact("sha256:1234567890123456789012345678901234567890123456789012345678901234"),
+					"pseudo_tag":    knownvalue.StringExact("unused@sha256:1234567890123456789012345678901234567890123456789012345678901234"),
+				})),
+			},
+		}},
+	})
+
+	// A shorthand digest ref string has everything (including a pseudo tag)
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{{
+			Config: `output "parsed" { value = provider::oci::parse("sample@sha256:1234567890123456789012345678901234567890123456789012345678901234") }`,
+			ConfigStateChecks: []statecheck.StateCheck{
+				statecheck.ExpectKnownOutputValue("parsed", knownvalue.ObjectExact(map[string]knownvalue.Check{
+					"registry":      knownvalue.StringExact("index.docker.io"),
+					"repo":          knownvalue.StringExact("library/sample"),
+					"registry_repo": knownvalue.StringExact("index.docker.io/library/sample"),
+					"digest":        knownvalue.StringExact("sha256:1234567890123456789012345678901234567890123456789012345678901234"),
+					"pseudo_tag":    knownvalue.StringExact("unused@sha256:1234567890123456789012345678901234567890123456789012345678901234"),
+				})),
+			},
+		}},
+	})
+
+	// A shorthand tagged and digest ref string has everything (including a replaced pseudo tag)
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+
+		Steps: []resource.TestStep{{
+			Config: `output "parsed" { value = provider::oci::parse("sample:cursed@sha256:1234567890123456789012345678901234567890123456789012345678901234") }`,
+			ConfigStateChecks: []statecheck.StateCheck{
+				statecheck.ExpectKnownOutputValue("parsed", knownvalue.ObjectExact(map[string]knownvalue.Check{
+					"registry":      knownvalue.StringExact("index.docker.io"),
+					"repo":          knownvalue.StringExact("library/sample"),
+					"registry_repo": knownvalue.StringExact("index.docker.io/library/sample"),
+					"digest":        knownvalue.StringExact("sha256:1234567890123456789012345678901234567890123456789012345678901234"),
+					"pseudo_tag":    knownvalue.StringExact("unused@sha256:1234567890123456789012345678901234567890123456789012345678901234"),
+				})),
+			},
+		}},
+	})
+}
